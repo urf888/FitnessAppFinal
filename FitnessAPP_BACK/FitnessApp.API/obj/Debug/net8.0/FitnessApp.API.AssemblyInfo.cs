0e61e813b35c683ee0e46f8f969a59683b1fd157
Pop Tudor <p.tudor38@yahoo.ro>
2025-05-05 19:46:20 +0300

    Initial upload - clean version

--- FitnessAPP_BACK/FitnessApp.API/obj/Debug/net8.0/FitnessApp.API.AssemblyInfo.cs
+++ FitnessAPP_BACK/FitnessApp.API/obj/Debug/net8.0/FitnessApp.API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FitnessApp.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3f8466d224cf40ed35f5b7a392e09166cebddfc1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a4a11321adab26eadd109c19bc8be015a3a605f4")]
 [assembly: System.Reflection.AssemblyProductAttribute("FitnessApp.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FitnessApp.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
